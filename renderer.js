21f71f2bf2999bcbb62e916e49fb6dcd269d65b0
Umer Khan <umerkk164@gmail.com>
2022-11-22 00:41:14 +0500

    memory added

--- renderer.js
+++ renderer.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
     let regsEl = document.getElementById("regs");
-    
+
     for(reg in regs) {
         let regEl = document.createElement("span");
         regEl.id = "r" + regs[reg];
@@ -9,5 +9,16 @@ window.addEventListener("load", () => {
         regsEl.appendChild(regEl);
     }
 
+    let memsEl = document.getElementById("mem");
+
+    for(let i = 0; i < 16; i++) {
+        let memEl = document.createElement("span");
+        memEl.id = "m" + i.toString(2);
+        memEl.classList.add("memory");
+        memEl.innerHTML = "0000";
+        memsEl.appendChild(memEl);
+    }
+
     document.body.appendChild(regsEl);
+    document.body.appendChild(memsEl);
 });
