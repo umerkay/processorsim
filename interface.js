7d964ae84fedd40f82198fb07b1f081e2a436ec8
Umer Khan <umerkk164@gmail.com>
2022-11-22 12:16:48 +0500

    New interface functions

--- interface.js
+++ interface.js
@@ -1,3 +1,15 @@
-function setRegValue(reg, value) {
-    document.getElementById("r" + regs[reg]).innerHTML = value;
+function getRegValue(reg) {
+    return document.getElementById("r" + regs[reg]).innerHTML;
+}
+
+function setRegValue(reg,value) {
+    document.getElementById("r"+regs[reg]).innerHTML = value;
+}
+
+function getMemValue(address) {
+    return document.getElementById("m" + address).innerHTML;
+}
+
+function setMemValue(address, value) {
+    document.getElementById("m"+address).innerHTML = value;
 }
--- renderer.js
+++ renderer.js
@@ -6,6 +6,7 @@ window.addEventListener("load", () => {
         regEl.id = "r" + regs[reg];
         regEl.classList.add("register");
         regEl.innerHTML = "0000";
+
         regsEl.appendChild(regEl);
     }
 
@@ -13,7 +14,7 @@ window.addEventListener("load", () => {
 
     for(let i = 0; i < 16; i++) {
         let memEl = document.createElement("span");
-        memEl.id = "m" + i.toString(2);
+        memEl.id = "m" + i.toString(2); 
         memEl.classList.add("memory");
         memEl.innerHTML = "0000";
         memsEl.appendChild(memEl);
