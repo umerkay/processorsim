70f3c5bba7c0533790ec1b24a3f100304def6e24
faizyabShah <faizyabshah0123@gmail.com>
2022-11-23 18:46:09 +0500

    MOV functions added

--- assembler.js
+++ assembler.js
@@ -1,46 +1,66 @@
 let code = 
-`MOV ax,BX
-MOV ax,@BX
-ADD BX,0009H`;
-//JMP
-//LOOP
+`MOV bx, 1234
+mov [ax], 1234
+MOV ax, [bx]`;
+
 
 function parseAssembly(text) {
+    console.log(text);
     let instructions = text.split("\n").filter(x => x != 0);
-    let bin = instructions.map((x,i) => instructionToMachine(x,i)); //err
-
     for(let i = 0; i < instructions.length; i++) {
         instructionToMachine(instructions[i],i);
     }
-
-    console.log(bin);
 }
 
 function parseOperand(op) {
-    return {
-        isMemory: true,
-        regORhex: "H",
-        code: "0089H",
-        length: 16
+    let result = {};
+    if (op[0] == '[') {
+        result.isMemory = true;
+        inner = op.match(/\[([^)]+)\]/)[1];
+        if (regs[inner] != undefined) {
+            result.regORhex = "R";
+            result.code = regs[inner].code;
+            result.length = regs[inner].length;
+        } else {
+            result.regORhex = "H";
+            result.code = inner;
+            result.length = 0;
+        }
+    } else {
+        result.isMemory = false;
+        if (regs[op] != undefined) {
+            result.regORhex = "R";
+            result.code = regs[op].code;
+            result.length = regs[op].length;
+        } else {
+            result.regORhex = "H";
+            result.code = op;
+            result.length = 0;
+        }
     }
+    return result;
+    
 }
 
 function instructionToMachine(instr, i) {
+    // console.log(instr);
     let op = instr.split(" ")[0].toUpperCase();
     let operands = instr.split(" ").slice(1).join("").toUpperCase().split(",");
 
-    operands.map(parseOperand);
-    
-    let D = "0"; //decide D
-    let W = "1"; //decide W
-    let MOD = "11"; //decide MOD
-    let Reg = "010"; //calc reg
-    let RsM = "111"; //calc RsM
+    let op1 = parseOperand(operands[0]);
+    let op2 = parseOperand(operands[1]);
+    console.log(instrSet[op].finalParse(op1, op2));
 
-    console.log(op, operands);
-    
-    if(instrSet[op] == undefined) console.error("Operation undefined at line " + i);
-    else return instrSet[op].opcode + D + W + MOD + Reg + RsM;
 }
 
+
+function hexToBinary(hex) {
+    let result;
+    result = parseInt(hex, 16).toString();
+    result = parseInt(result).toString(2);
+    while (result.length < 16) {
+        result = "0" + result;
+    }
+    return result;
+}
 parseAssembly(code);
